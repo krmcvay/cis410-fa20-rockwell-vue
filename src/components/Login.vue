494e2908c4e1d42be7a2f6aff078c6b5d63e47f3
krmcvay <krmcvay@rams.colostate.edum>
2020-12-03 23:58:12 -0700

    13.2 11/19 axios, signup cont'd

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Login</h1>
-        <div class="alert alert-success">
+        <div v-if="this.$route.query.loginsuccess" class="alert alert-success">
             Thanks for signing up. Please log in now. 
         </div>
     </div>
